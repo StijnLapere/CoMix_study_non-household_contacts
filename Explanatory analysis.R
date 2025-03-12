1fd5e41a5d0286e3ff9bf9f5cb616bbea1de246d
StijnLapere <stijn.lapere@student.uhasselt.be>
2025-03-12 17:45:26 +0100

    Explanatory analysis.R

--- Explanatory analysis.R
+++ Explanatory analysis.R
@@ -1,192 +1,3 @@
-library(data.table)
-contact_extra <- fread("C:/Users/stijn/OneDrive/Documenten/CoMix_BE_contact_extra.csv",fill=TRUE)
-
-library(dplyr)
-minmaxcontacts <- contact_extra %>%
-  group_by(wave, part_uid) %>%
-  summarise(count = n()) %>% 
-  group_by(wave) %>%
-  summarise(
-    min_count = min(count),
-    max_count = max(count)
-  )
-
-print(minmaxcontacts,n=32)
-
-participantmaxcount <- contact_extra %>%
-  group_by(wave, part_uid) %>%
-  summarise(count = n()) %>% 
-  group_by(wave) %>%
-  filter(count == max(count)) %>%
-  ungroup()
-
-print(participantmaxcount,n=32)
-
-
-library(tidyr)
-
-counts_per_wave <- contact_extra %>%
-  group_by(part_uid, wave) %>%
-  summarise(count = n(), .groups = "drop") %>%  
-  pivot_wider(names_from = wave, values_from = count, values_fill = list(count = 0))
-
-
-
-# calculate total number of waves participant participated
-contactextrawaves <- contact_extra %>%
-  group_by(part_uid) %>%
-  mutate(num_waves_participated = n_distinct(wave))
-
-contactextrawaves_short <- contactextrawaves %>%
-  group_by(part_uid, wave) %>%
-  summarise(n_contacts = n(), num_househ_cont = sum(cnt_household), num_nonhouseh_cont = n_contacts-num_househ_cont, .groups = "drop")
-
-contactextrawaves_short <- contactextrawaves_short %>%
-  mutate(part_uid_wave = as.integer(paste0(substr(part_uid, 4, nchar(part_uid)), sprintf("%02d", wave))))
-
-participant_extra_columns <- participant_extra %>% select("part_id","part_social_group_be","part_vacc","part_elevated_risk","part_face_mask","part_symp_none","area_3_name")
-
-contactextrawaves_merged <- contactextrawaves_short %>%
-  left_join(participant_extra_columns, by = c("part_uid_wave" = "part_id"))
-
-comix_be_sday_columns <- sday %>% select("part_id","holiday")
-
-contactextrawaves_merged <- contactextrawaves_merged %>%
-  left_join(comix_be_sday_columns, by = c("part_uid_wave" = "part_id"))
-
-participant_common_columns <- participant_common %>% select("part_id","part_age","part_gender")
-
-contactextrawaves_merged <- contactextrawaves_merged %>%
-  left_join(participant_common_columns, by = c("part_uid_wave" = "part_id"))
-
-contactextrawaves_merged <- contactextrawaves_merged %>%
-  mutate(part_uid_wave_HH = as.character(paste0("HH", part_uid_wave)))
-
-hh_common_columns <- hh_common %>% select("hh_id","hh_size")
-
-contactextrawaves_merged <- contactextrawaves_merged %>%
-  left_join(hh_common_columns, by = c("part_uid_wave_HH" = "hh_id"))
-
-contact_extra_columns <- contactextrawaves %>%
-  arrange(part_uid, wave) %>%  
-  group_by(part_uid, wave) %>%
-  slice(1) %>%  # Keep only the first row per (part_uid, wave)
-  ungroup() %>% select("part_uid","wave","weekday","date","num_waves_participated")
-
-contactextrawaves_merged <- contactextrawaves_merged %>%
-  left_join(contact_extra_columns, by = c("part_uid","wave"))
-
-contactextrawaves_merged <- contactextrawaves_merged %>%
-  mutate(adult_cat = case_when(
-    part_age >= 0 & part_age <= 17 ~ "Children",
-    part_age >= 18 & part_age <= 69 ~ "Adult",
-    part_age >= 70 ~ "Elderly",
-    TRUE ~ NA_character_  # Handle missing or unexpected values
-  ))
-
-contactextrawaves_merged <- contactextrawaves_merged %>%
-  mutate(adult = case_when(
-    adult_cat == "Adult" ~ 1,
-    adult_cat == "Elderly" ~ 0,
-    adult_cat == "Children" ~ 0,
-    TRUE ~ NA_real_  # Handle missing or unexpected values
-  ))
-
-contactextrawaves_merged <- contactextrawaves_merged %>%
-  mutate(elderly = case_when(
-    adult_cat == "Adult" ~ 0,
-    adult_cat == "Elderly" ~ 1,
-    adult_cat == "Children" ~ 0,
-    TRUE ~ NA_real_  # Handle missing or unexpected values
-  ))
-
-contactextrawaves_merged <- contactextrawaves_merged %>%
-  mutate(wd = case_when(
-    weekday == "Monday" ~ "Weekday",
-    weekday == "Tuesday" ~ "Weekday",
-    weekday == "Wednesday" ~ "Weekday",
-    weekday == "Thursday" ~ "Weekday",
-    weekday == "Friday" ~ "Weekday",
-    weekday == "Saturday" ~ "Weekend",
-    weekday == "Sunday" ~ "Weekend",
-    TRUE ~ NA_character_  # Handle missing or unexpected values
-  ))
-
-contactextrawaves_merged <- contactextrawaves_merged %>%
-  mutate(area_3_name = recode(area_3_name,
-                              "R\xe9gion Flamande / Vlaams Gewest" = "Vlaams Gewest",
-                              "R\xe9gion Wallonne / Waals Gewest" = "Waals Gewest",
-                              "R\xe9gion de Bruxelles-Capitale / Brussels Hoofdstede" = "Brussels Hoofdstede"))
-
-table(contactextrawaves_merged$hh_size) #Make factor variable with hhsize in {1,2,3,4+}
-contactextrawaves_merged <- contactextrawaves_merged %>%
-  mutate(hhsize_cat = as.factor(ifelse(hh_size == 1, "1",
-                                       ifelse(hh_size == 2, "2",
-                                              ifelse(hh_size == 3, "3","4+")))))
-
-contactextrawaves_merged2 <- contactextrawaves_merged %>%  
-  group_by(part_uid) %>% 
-  arrange(part_uid) %>%
-  # add column wavecount which represents the n-th wave for each participant
-  mutate(wavecount = as.factor(ifelse(row_number() == 1, "1",
-                                     ifelse(row_number() == 2, "2",
-                                            ifelse(row_number() == 3, "3",
-                                                   ifelse(row_number() == 4, "4",
-                                                          ifelse(row_number() == 5, "5",
-                                                                 ifelse(row_number() == 6, "6",
-                                                                        ifelse(row_number() == 7, "7", ">=8"))))))))) %>% 
-  arrange(part_uid) %>% group_by(part_uid) %>%  as.data.frame()
-
-start_date <- as.Date("2020-12-23")
-
-finaldataset <- contactextrawaves_merged2 %>%
-  mutate(day_number = as.numeric(date - start_date) / 365)
-
-finaldataset$part_uid <- as.factor(finaldataset$part_uid)
-finaldataset$wave <- as.factor(finaldataset$wave)
-finaldataset$part_social_group_be <- as.factor(finaldataset$part_social_group_be)
-finaldataset$part_social_group_be <- relevel(finaldataset$part_social_group_be, ref = "Group 1&2")
-finaldataset$part_vacc <- factor(finaldataset$part_vacc, levels = c("Yes", "No"))
-finaldataset$part_vacc <- relevel(finaldataset$part_vacc, ref = "No")
-finaldataset$part_elevated_risk <- factor(finaldataset$part_elevated_risk, levels = c("yes", "no"))
-finaldataset$part_elevated_risk <- relevel(finaldataset$part_elevated_risk, ref = "no")
-finaldataset$part_face_mask <- factor(finaldataset$part_face_mask, levels = c("yes", "no"))
-finaldataset$part_face_mask <- relevel(finaldataset$part_face_mask, ref = "no")
-finaldataset$part_symp_none <- case_when(finaldataset$part_symp_none == 0 ~ "No",finaldataset$part_symp_none == 1 ~ "Yes")
-finaldataset$part_symp_none <- factor(finaldataset$part_symp_none, levels = c("No", "Yes"))
-finaldataset$part_symp_none <- relevel(finaldataset$part_symp_none, ref = "No")
-finaldataset$area_3_name <- factor(finaldataset$area_3_name, levels = c("Vlaams Gewest", "Waals Gewest", "Brussels Hoofdstede"))
-finaldataset$area_3_name <- relevel(finaldataset$area_3_name, ref = "Brussels Hoofdstede")
-finaldataset$holiday <- case_when(finaldataset$holiday == 0 ~ "No",finaldataset$holiday == 1 ~ "Yes")
-finaldataset$holiday <- factor(finaldataset$holiday, levels = c("No", "Yes"))
-finaldataset$holiday <- relevel(finaldataset$holiday, ref = "No")
-finaldataset$part_gender <- factor(finaldataset$part_gender, levels = c("M","F"))
-finaldataset$part_gender <- relevel(finaldataset$part_gender, ref = "F")
-finaldataset$hhsize_cat <- relevel(finaldataset$hhsize_cat, ref = "1")
-finaldataset$adult_cat <- factor(finaldataset$adult_cat, levels = c("Adult","Children","Elderly"))
-finaldataset$adult_cat <- relevel(finaldataset$adult_cat, ref = "Adult")
-finaldataset$adult <- factor(finaldataset$adult, levels = c("0","1"))
-finaldataset$adult <- relevel(finaldataset$adult, ref = "0")
-finaldataset$elderly <- factor(finaldataset$elderly, levels = c("0","1"))
-finaldataset$elderly <- relevel(finaldataset$elderly, ref = "0")
-finaldataset$wd <- factor(finaldataset$wd, levels = c("Weekday","Weekend"))
-finaldataset$wd <- relevel(finaldataset$wd, ref = "Weekday")
-finaldataset$wavecount <- relevel(finaldataset$wavecount, ref = "1")
-
-  
-finaldataset_children <- finaldataset %>%
-  filter(adult_cat == "Children")
-
-finaldataset_adult <- finaldataset %>%
-  filter(adult_cat == "Adult")
-
-finaldataset_elderly <- finaldataset %>%
-  filter(adult_cat == "Elderly")
-
-################################################################################
-############################### Exploratory analysis ###########################
-################################################################################
-
 ### Number of participants
 length(unique(finaldataset$part_uid)) #3977
 
@@ -195,6 +6,29 @@ nrow(finaldataset) #32740
 
 ### Total number of reported contacts
 sum(finaldataset$n_contacts) #166380
+
+library(dplyr)
+minmaxcontacts <- participant_extra %>%
+  group_by(wave, part_uid) %>%
+  summarise(count = n_cnt_all) %>% 
+  group_by(wave) %>%
+  summarise(
+    min_count = min(count),
+    max_count = max(count)
+  )
+
+print(minmaxcontacts,n=32)
+
+participantmaxcount <- participant_extra %>%
+  group_by(wave, part_uid) %>%
+  summarise(count = n_cnt_all) %>% 
+  group_by(wave) %>%
+  filter(count == max(count)) %>%
+  ungroup()
+
+print(participantmaxcount,n=32)
+
+
 
 ### Relative percentages of age category participation per wave
 contactextrawaves_waveage <- finaldataset %>%
